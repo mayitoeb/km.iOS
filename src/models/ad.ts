09ab1927214cf0f3d37493353b33b94d9e1fefd9
mayitoeb <mayito.eb@gmail.com>
2018-04-06 22:53:30 -0400

    ad model and ad service

--- src/models/ad.ts
+++ src/models/ad.ts
@@ -1,4 +1,4 @@
-export interface IAd {
+export class Ad {
     id: string;
     title: string;
     subtitle: string;
---
+++ src/services/ad.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError, map, tap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+
+import {Ad} from '../models/ad'
+
+export class AdService {
+    private adUrl = 'http://kentumuromobileappdev.azurewebsites.net/tables/ad'; 
+
+    constructor(private http: HttpClient) { }
+
+    getAds (): Observable<Ad[]> {
+        return this.http.get<Ad[]>(this.adUrl)
+        .pipe(
+            tap(heroes => console.log('fetched ads')),
+            catchError(this.handleError('getAds', []))
+        );
+    }
+
+    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    private handleError<T> (operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+    
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+        
+            // TODO: better job of transforming error for user consumption
+            //this.log(`${operation} failed: ${error.message}`);
+        
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
+}
